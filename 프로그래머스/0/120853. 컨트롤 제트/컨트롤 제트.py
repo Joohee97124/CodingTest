5addf1bfb42981faf09cbe38e5ea009c83f2396c
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-07-11 15:22:57 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,16 +1,15 @@
 def solution(s):
-    #answer = 0
+    answer = 0
     array = s.split(" ")
     
     final = []
     for i,arr in enumerate(array):
         if arr=='Z':
-            final.pop(-1)
+            answer -= int(array[i-1])
+            #final.pop(-1)
         else:
-            final.append(int(arr))
-    
-    #for fin in final:
-    #    answer += fin
-    #answer = sum([fin for fin in final])
-        
-    return sum([fin for fin in final])
+            answer += int(arr)
+            #final.append(int(arr))
+            
+    return answer    
+    #return sum([fin for fin in final])
