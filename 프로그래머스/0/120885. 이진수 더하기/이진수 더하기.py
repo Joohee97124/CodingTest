d344e136801e337aa3f7714dd729a35b5444eee7
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-07-23 14:00:11 +0900

    [level 0] Title: 이진수 더하기, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
@@ -0,0 +1,7 @@
+def solution(bin1, bin2):
+    #answer = ''
+    #int1 = int(bin1, 2)
+    #int2 = int(bin2, 2)
+    
+    #answer = bin(int(bin1, 2) + int(bin2, 2))[2:]
+    return bin(int(bin1, 2) + int(bin2, 2))[2:]
