ae3cf2aa42e8123d043ae407e4776ce3f6e3a2a6
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-07-26 19:51:00 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,16 +1,14 @@
 def solution(score):
-    answer = []
+    #answer = []
     
     temp = [(eng+math)/2 for eng,math in score]
     #for eng,math in score:
     #    temp.append( (eng+math)//2 )
     
     temp.sort(reverse=True)
-    print(temp)
     
-    for eng,math in score:
-        answer.append(temp.index((eng+math)/2)+1)
-    print(answer)
-    #answer = [temp.index((eng+math)//2)+1 for eng,math in score]
+    #for eng,math in score:
+    #    answer.append(temp.index((eng+math)/2)+1)
+    #answer = [temp.index((eng+math)/2)+1 for eng,math in score]
     
-    return answer
+    return [temp.index((eng+math)/2)+1 for eng,math in score]
