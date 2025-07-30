8ea5ac7e61818d2d6af5b6ea716174c7d10eb72c
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-07-30 14:20:48 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -0,0 +1,6 @@
+def solution(start_num, end_num):
+    #answer = []
+    #for i in range(start_num, end_num-1, -1):
+    #    answer.append(i)
+    #answer = [i for i in range(start_num, end_num-1, -1)]
+    return [i for i in range(start_num, end_num-1, -1)]
