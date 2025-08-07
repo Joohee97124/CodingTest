3abac8360e6d4443772002f601e10120d3a85fdb
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-08-07 13:08:16 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,14 @@
+def solution(date1, date2):
+    y1,m1,d1 = date1
+    y2,m2,d2 = date2
+    
+    if y1 < y2 : 
+        return 1
+    
+    if y1==y2 and m1<m2:
+        return 1
+    if y1==y2 and m1==m2 and d1<d2:
+        return 1
+    return 0
+    
+    return 1
