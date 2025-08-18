3aeb08314e7dbb71ae7f4c5613f4eb1a4a41c7a6
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-08-18 10:22:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.94 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,11 @@
 def solution(participant, completion):
-    dic = {}
-    for par in participant:
-        dic[par] = dic.get(par, 0) +1
+    person = {}
+    for part in participant :
+        person[part] = person.get(part,0) +1
     
     for com in completion:
-        dic[com] -= 1  
+        person[com] -= 1
     
-    for k,v in dic.items():
-        if v > 0:
+    for k,v in person.items():
+        if v > 0 :
             return k
