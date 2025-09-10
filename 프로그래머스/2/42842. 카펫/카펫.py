4253a9da48b597bfa53e3e2d0b4adb3990b5e2ff
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-09-10 14:17:56 +0900

    [level 2] Title: 카펫, Time: 0.16 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow
+    tmp = []
+    
+    for i in range(2,int(total ** 0.5)+1):
+        if total%i == 0:
+            tmp.append([i,total//i])
+    
+    print(tmp)
+    for a,b in tmp:
+        if (a-2)*(b-2)==yellow:
+            answer.extend([b,a])
+            
+    
+    return answer
