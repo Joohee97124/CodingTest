1619a91c1f89c5912dbf37bbf9f94d99ca1405a3
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-09-16 21:09:43 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.37 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,16 @@
+def solution(n, lost, reserve):
+    lost.sort()
+    reserve.sort()
+    
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve)-set(lost)
+    
+    for l in list(lost_set):
+        if (l-1) in reserve_set:
+            lost_set.remove(l)
+            reserve_set.remove(l-1)
+        elif (l+1) in reserve_set:
+            lost_set.remove(l)
+            reserve_set.remove(l+1)
+    
+    return n - len(lost_set)
