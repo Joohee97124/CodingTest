b285ea58b6c6a662dccab4afb498d0b732e7e9a5
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-09-19 15:47:18 +0900

    [level 2] Title: 구명보트, Time: 10.07 ms, Memory: 9.59 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,16 +2,16 @@ def solution(people, limit):
     answer = 0
     people.sort(reverse=True)
     
-    # 80 70 50 50
     point1 = 0
     point2 = len(people)-1
+    
     while True:
-        if point1 > point2:
+        if point1 > point2 :
             break
-        
-        if (people[point1] + people[point2]) <= limit:
+        if people[point1] + people[point2] <= limit:
             point2 -= 1
         answer += 1
         point1 += 1
+            
     
     return answer
