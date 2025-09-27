837bba95e3648df5de361a16fa12e0fd0b3ec2b4
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-09-27 15:15:50 +0900

    [level 1] Title: 폰켓몬, Time: 1.38 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,8 @@
 def solution(nums):
-    phone = {}
-    for num in nums:
-        phone[num] = phone.get(num,0) +1
+    k = len(nums)//2
     
-    return len(phone) if (len(nums)//2) > len(phone) else (len(nums)//2)
+    pocketmon = {}
+    for num in nums:
+        pocketmon[num] = pocketmon.get(num,0)+1
+    
+    return k if k < len(pocketmon) else len(pocketmon)
