2b5ae1a02957b3efdde4f4376875b8a7c6d7f4f5
정주희 <70319866+Joohee97124@users.noreply.github.com>
2025-09-27 15:50:28 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,9 @@
 def solution(clothes):
     answer = 1
-    hanger={}
-    for a,b in clothes:
-        hanger[b] = hanger.get(b,0) +1
+    clothes_set = {}
+    for cloth,types in clothes:
+        clothes_set[types] = clothes_set.get(types,0)+1
     
-    for k,v in hanger.items():
-        answer *= (v+1)
-    
+    for cloth in clothes_set.values():
+        answer *= (cloth+1)
     return answer-1
